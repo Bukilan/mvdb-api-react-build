2834c74faab0c2d56357b304314dcac87697ade0
Bukilan <32181180+Bukilan@users.noreply.github.com>
2019-08-08 20:17:49 +0300

    inital commit

---
+++ precache-manifest.1057ceafe6af3c83fba04edc4e3fe12f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cb3dd8036e0108e994061441da1fc09",
+    "url": "/mvdb-api-react-build/static/media/image2vector.7cb3dd80.svg"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/mvdb-api-react-build/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "41994d44eb6ccdecd639",
+    "url": "/mvdb-api-react-build/static/js/runtime~main.d0363bd3.js"
+  },
+  {
+    "revision": "3ef888c25164154f06a0",
+    "url": "/mvdb-api-react-build/static/js/main.bf0063f4.chunk.js"
+  },
+  {
+    "revision": "6ab0b9ae0c443781472e",
+    "url": "/mvdb-api-react-build/static/js/2.b91d4d91.chunk.js"
+  },
+  {
+    "revision": "3ef888c25164154f06a0",
+    "url": "/mvdb-api-react-build/static/css/main.9a1f422f.chunk.css"
+  },
+  {
+    "revision": "9485b2e4db5734c65bded2bb97f650b5",
+    "url": "/mvdb-api-react-build/index.html"
+  }
+];
